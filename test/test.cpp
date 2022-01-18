7daaaa8f59512c0bcb25de9e217da80d763a1b7f
Oleg Vorobiov <oleg.vorobiov@hobovrlabs.org>
2022-01-18 23:34:47 +0200

    test: Added a new test & fixed assert of the old one
    Signed-off-by: Oleg Vorobiov <oleg.vorobiov@hobovrlabs.org>

--- test/test.cpp
+++ test/test.cpp
@@ -5,85 +5,83 @@
 
 #include <iostream>
 #include "lazy_sockets.h"
-#include "errno.h"
+#include <errno.h>
 
 #include <thread>
 
-#define ASSERT_RES(res, msg, id, err) \
+#define ASSERT_RES(res) \
 	if ((res)) { \
-		printf(msg, id, err); \
-		throw std::runtime_error("assert on " + std::to_string(__LINE__)); \
+		throw std::runtime_error("assert on line: " + std::to_string(__LINE__) + ", reason: " + std::to_string(errno)); \
 	} \
 	else (void)0
 
 
 void server() {
-	lsc::LScoket<AF_INET, SOCK_STREAM, 0> binder;
+	lsc::LSocket<AF_INET, SOCK_STREAM, 0> binder;
 
 	int res = binder.Bind("0.0.0.0", 6969);
-	ASSERT_RES(res, "%d failed to bind: %d", 0, errno);
+	ASSERT_RES(res);
 
 	res = binder.Listen(2);
-	ASSERT_RES(res, "%d failed to listen: %d", 0, errno);
+	ASSERT_RES(res);
 
 	// we can ditch the binder now and create the client socket
 
 	res = binder.Accept();
-	ASSERT_RES(res < 0, "%d failed to accept: %d", 0, errno);
+	ASSERT_RES(res < 0);
 
-	lsc::LScoket<AF_INET, SOCK_STREAM, 0> client1(res, lsc::EStat_connected);
+	lsc::LSocket<AF_INET, SOCK_STREAM, 0> client1(res, lsc::EStat_connected);
 
 	res = binder.Accept();
-	ASSERT_RES(res < 0, "%d failed to accept: %d", 0, errno);
+	ASSERT_RES(res < 0);
 
-	lsc::LScoket<AF_INET, SOCK_STREAM, 0> client2(res, lsc::EStat_connected);
+	lsc::LSocket<AF_INET, SOCK_STREAM, 0> client2(res, lsc::EStat_connected);
 
 	char msg[] = "hello there :P";
 
 	res = client1.Send(msg, strlen(msg));
-	ASSERT_RES(res < 0, "%d failed to send: %d", 0, errno);
+	ASSERT_RES(res < 0);
 
 	std::cout << "binder sent: " << msg << '\n';
 
 	res = client2.Send(msg, strlen(msg));
-	ASSERT_RES(res < 0, "%d failed to send: %d", 0, errno);
+	ASSERT_RES(res < 0);
 
 	std::cout << "binder sent: " << msg << '\n';
 
 	char recv_buff[256];
 
 	res = client1.Recv(recv_buff, sizeof(recv_buff));
-	ASSERT_RES(res < 0, "%d failed to recv: %d", 0, errno);
+	ASSERT_RES(res < 0);
 
 	std::cout << "binder got: " << recv_buff << '\n';
 
 	res = client2.Recv(recv_buff, sizeof(recv_buff));
-	ASSERT_RES(res < 0, "%d failed to recv: %d", 0, errno);
+	ASSERT_RES(res < 0);
 
 	std::cout << "binder got: " << recv_buff << '\n';
 }
 
 void client(int id, std::string msg) {
-	lsc::LScoket<AF_INET, SOCK_STREAM, 0> conn;
+	lsc::LSocket<AF_INET, SOCK_STREAM, 0> conn;
 
 	int res = conn.Connect("127.0.0.1", 6969);
-	ASSERT_RES(res, "%d failed to connect: %d", id, errno);
+	ASSERT_RES(res);
 
 	char conn_recv[256];
 
 	res = conn.Recv(conn_recv, sizeof(conn_recv));
-	ASSERT_RES(res < 0, "%d failed to recv: %d", id, errno);
+	ASSERT_RES(res < 0);
 
 	std::cout << "conn" << id << " got: " << conn_recv << '\n';
 
 	res = conn.Send(msg.c_str(), msg.size());
-	ASSERT_RES(res < 0, "%d failed to send: %d", id, errno);
+	ASSERT_RES(res < 0);
 
 	std::cout << "conn" << id << " sent: " << msg << '\n';
 }
 
-int main()
-{
+int main() {
 	
 	std::thread a(server);
 	std::this_thread::sleep_for(std::chrono::milliseconds(1)); // let the server setup first
---
+++ test/test2.cpp
@@ -0,0 +1,125 @@
+// SPDX-License-Identifier: GPL-2.0-only
+
+// Copyright (C) 2020-2021 Oleg Vorobiov <oleg.vorobiov@hobovrlabs.org>
+
+
+#include <iostream>
+#include "lazy_sockets.h"
+#include <errno.h>
+
+#include <thread>
+
+#define ASSERT_RES(res) \
+	if ((res)) { \
+		throw std::runtime_error("assert on line: " + std::to_string(__LINE__) + ", reason: " + std::to_string(errno)); \
+	} \
+	else (void)0
+
+using namespace lsc;
+
+void server_echo(bool* echo_alive) {
+	// lets create a socket
+	LSocket<AF_INET, SOCK_STREAM, 0> binder;
+
+	// bind it
+	int res = binder.Bind("0.0.0.0", 6969);
+	ASSERT_RES(res);
+
+
+	// make it a static listener
+	res = binder.Listen(1);
+	ASSERT_RES(res);
+
+	// and accept a single connection
+	res = binder.Accept();
+	ASSERT_RES(res < 0);
+
+	// convert that connection from a raw socket to an LScocket and profit
+	LSocket<AF_INET, SOCK_STREAM, 0> client(res, EStat_connected);
+
+	char buff[256]; // receive buffer
+
+	while (*echo_alive) {
+		// receive data
+		res = client.Recv(buff, sizeof(buff));
+		ASSERT_RES(res < 0);
+
+		// re send what we received
+		res = client.Send(buff, res);
+		ASSERT_RES(res < 0);
+	}
+
+	// all socked will be closed on class instance destruction
+}
+
+struct my_tag {
+	char a;
+	char b;
+	char c;
+	char d;
+};
+
+static constexpr int MSG_SIZE = 1024 * 9;
+
+void call_back(void* buff, size_t len) {
+
+	std::cout << '\'';
+
+	for (int i=MSG_SIZE / 4  - 20; i < len / 4; i++)
+		std::cout << (((int*)buff)[i]) << ',';
+
+	std::cout << "'<- " << len / 4 << "\n";
+
+	// memset(buff, 0, len + sizeof(my_tag));
+}
+
+
+
+int main() {
+	bool echo_alive = true;
+	std::thread a(server_echo, &echo_alive);
+
+	std::this_thread::sleep_for(std::chrono::milliseconds(1)); // let the server setup first
+
+	LSocket<AF_INET, SOCK_STREAM, 0> client;
+
+	int res = client.Connect("127.0.0.1", 6969);
+	ASSERT_RES(res);
+
+	my_tag tag = {'\r', '\t', '\n', '\0'};
+
+	ThreadedRecvLoop<AF_INET, SOCK_STREAM, 0, my_tag> recv_loop(&client, &tag, call_back, 2);
+
+	recv_loop.Start();
+
+	void* msg = malloc(MSG_SIZE + sizeof(my_tag));
+	// for (int i=0; i<MSG_SIZE; msg[i]=i, i++);
+	int* tmp = (int*)msg;
+	for (int i=0; i < MSG_SIZE / 4; tmp[i]=i, i++);
+
+	memcpy((char*)msg + MSG_SIZE, &tag, sizeof(tag));
+
+
+	// send loop
+
+	for (int i=0; i<10; i++) {
+		res = client.Send(msg, MSG_SIZE + sizeof(my_tag));
+		ASSERT_RES(res < 0);
+	
+		std::this_thread::sleep_for(std::chrono::milliseconds(2)); // let the server setup first
+	}
+
+	recv_loop.Stop();
+
+	echo_alive = false;
+
+	// to unblock the recv wait in the server thread
+	res = client.Send(msg, MSG_SIZE + sizeof(my_tag));
+	ASSERT_RES(res < 0);
+
+	a.join();
+
+	std::cout << "done\n";
+
+	return 0;
+}
