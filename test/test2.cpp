7a8ae524e90a8db5f1f18743d756d329681fe349
Oleg Vorobiov <oleg.vorobiov@hobovrlabs.org>
2022-02-06 21:14:17 +0200

    test: test2.cpp: Clarified receiver usage
    The interface changed a bit, so this example needed
    to be changed accordingly.
    
    Signed-off-by: Oleg Vorobiov <oleg.vorobiov@hobovrlabs.org>

--- test/test2.cpp
+++ test/test2.cpp
@@ -17,9 +17,11 @@
 
 using namespace lsc;
 
+using tcp_socket = LSocket<AF_INET, SOCK_STREAM, 0>;
+
 void server_echo(bool* echo_alive) {
 	// lets create a socket
-	LSocket<AF_INET, SOCK_STREAM, 0> binder;
+	tcp_socket binder;
 
 	// bind it
 	int res = binder.Bind("0.0.0.0", 6969);
@@ -63,7 +65,7 @@ static constexpr int MSG_SIZE = 1024 * 9;
 
 void call_back(void* buff, size_t len) {
 
-	std::cout << '\'';
+	std::cout << "'...,";
 
 	for (int i=MSG_SIZE / 4  - 20; i < len / 4; i++)
 		std::cout << (((int*)buff)[i]) << ',';
@@ -77,18 +79,20 @@ void call_back(void* buff, size_t len) {
 
 int main() {
 	bool echo_alive = true;
-	std::thread a(server_echo, &echo_alive);
+	std::thread echo_thread(server_echo, &echo_alive);
 
-	std::this_thread::sleep_for(std::chrono::milliseconds(1)); // let the server setup first
+	// let the server setup first
+	std::this_thread::sleep_for(std::chrono::milliseconds(1));
 
-	LSocket<AF_INET, SOCK_STREAM, 0> client;
 
-	int res = client.Connect("127.0.0.1", 6969);
+	auto client = std::make_shared<tcp_socket>();
+
+	int res = client->Connect("127.0.0.1", 6969);
 	ASSERT_RES(res);
 
 	my_tag tag = {'\r', '\t', '\n', '\0'};
 
-	ThreadedRecvLoop<AF_INET, SOCK_STREAM, 0, my_tag> recv_loop(&client, &tag, call_back, 2);
+	ThreadedRecvLoop<AF_INET, SOCK_STREAM, 0, my_tag> recv_loop(client, tag, call_back, 2);
 
 	recv_loop.Start();
 
@@ -103,21 +107,25 @@ int main() {
 	// send loop
 
 	for (int i=0; i<10; i++) {
-		res = client.Send(msg, MSG_SIZE + sizeof(my_tag));
+		res = client->Send(msg, MSG_SIZE + sizeof(my_tag));
 		ASSERT_RES(res < 0);
 	
 		std::this_thread::sleep_for(std::chrono::milliseconds(2)); // let the server setup first
 	}
 
-	recv_loop.Stop();
 
 	echo_alive = false;
 
 	// to unblock the recv wait in the server thread
-	res = client.Send(msg, MSG_SIZE + sizeof(my_tag));
+	res = client->Send(msg, MSG_SIZE + sizeof(my_tag));
 	ASSERT_RES(res < 0);
 
-	a.join();
+	// releasing ownership of the receiver socket shared_ptr
+	// will cause the receiver to exit it's thread
+	client.reset();
+
+	recv_loop.Stop(); // call it to verify receiver thread is joined
+	echo_thread.join();
 
 	std::cout << "done\n";
 
