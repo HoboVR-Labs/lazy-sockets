36b58a72ef764c4d54b9ea70604071da6eac53f9
Oleg Vorobiov <oleg.vorobiov@hobovrlabs.org>
2022-02-06 20:52:26 +0200

    src: defines.h: Added ioctl, fixed error defines
    LSocket will support ioctls now. Also fixed error handling
    being accessed differently on different platforms.
    
    Signed-off-by: Oleg Vorobiov <oleg.vorobiov@hobovrlabs.org>

--- src/defines.h
+++ src/defines.h
@@ -8,23 +8,28 @@
 #define __LAZY_SOCKETS_VERSION "0.1.4"
 #define __LAZY_SOCKETS_BUILD 1642888800  // build date 2022 01 23
 
-#include <errno.h>
 
 // platform defined types
 #ifdef LINUX
+
+#include <sys/ioctl.h>
 
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <unistd.h>
 #include <netdb.h>
+#include <errno.h>
 
 #include <arpa/inet.h>
 
+namespace lsc {
+
 // pass through for the socket() call
 #define lsocket socket
+
 #define LSOCK_ERR -1
 
-namespace lsc {
+#define lerrno errno
 
 typedef struct sockaddr_in lcsockaddr_in;
 
@@ -42,30 +47,35 @@ typedef int lsocket_t;
 // Need to link with Ws2_32.lib
 #pragma comment(lib, "Ws2_32.lib")
 
+namespace lsc {
+
+using socklen_t = int;
+
 // cuz on windows you can't have nice things
 #define MSG_DONTWAIT 0
-#define socklen_t int
 #define LSOCK_ERR INVALID_SOCKET
 
-namespace lsc {
+#define lerrno WSAGetLastError()
 
 typedef struct sockaddr_in lcsockaddr_in;
 
 typedef SOCKET lsocket_t;
 
-static bool __winsock2_wsa_initialized = false;
+static bool __wsa_initialized = false;
+static unsigned int  __wsa_initialized_count = 0;
 
 // cuz on windows sockets require wsaStartup, call WSACleanup() yourself later, idc
 inline lsocket_t lsocket(int family, int type, int protocol) {
-	if (!__winsock2_wsa_initialized) {
+	if (!__wsa_initialized) {
 		WSADATA wsaData;
 		int res = WSAStartup(MAKEWORD(2, 2), &wsaData);
 		if (res != NO_ERROR) {
-			errno = res;
+			// errno = res;
 			return INVALID_SOCKET;
 		}
-		__winsock2_wsa_initialized = true;
+		__wsa_initialized = true;
 	}
+	__wsa_initialized_count++;
 	return socket(family, type, protocol);
 }
 
@@ -76,7 +86,22 @@ inline lsocket_t accept4(lsocket_t soc, struct sockaddr* addr, socklen_t* addrle
 }
 
 // cuz on windows close() doesn't exist
-#define close closesocket
+inline int close(lsocket_t soc) {
+	int res = closesocket(soc); // cache close result
+
+	// de init WSA on last socket close
+	if (!(--__wsa_initialized_count)) {
+		WSACleanup();
+		__wsa_initialized = false;
+	}
+
+	return res;
+}
+
+
+// cuz windows wants to be special
+using ioctl = ioctlsocket;
+
 
 }  // namespace lsc
 
